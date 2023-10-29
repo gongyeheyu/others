60f50221fa5d353a9c7ec16495c8175c533b9888
GONGYE Heyu <gongyeheyu@outlook.com>
2023-10-29 03:50:54 +0000

    up

--- code/count/count.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int i;
-    for(i=0; i<=100; i++){
-        cout << i << endl;
-}
-}
--- code/count/count.java
+++
@@ -1,8 +0,0 @@
-class count {
-    public static void main(String args[]){
-        int i;
-        for(i=0; i<=100; i++){
-            System.out.println(i);
-        }
-    }
-}
