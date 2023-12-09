e846fea21c8fc70b76281d4142ba74325ecdc66c
gongyeheyu <gongyeheyu@outlook.com>
2023-12-09 14:48:28 +0000

    gy3's first commit

--- code/compute_pi.py
+++ code/compute_pi.py
@@ -4,10 +4,7 @@ from time import perf_counter
 def calPI(N = 100):
     hits = 0
     start = perf_counter()
-    con=0
     for i in range(1, N*N+1):
-        con=con+1
-        print(con)
         x, y = random(), random()
         dist = pow(x ** 2 + y ** 2, 0.5)
         if dist <= 1.0:
@@ -16,6 +13,6 @@ def calPI(N = 100):
     use_time = perf_counter() - start
     return pi, use_time
  
-PI, use_time = calPI(100000)
+PI, use_time = calPI(10000)
 print('use Monte Carlo method to calculate PI: {}'.format(PI))
 print('use time: {} s'.format(use_time))
