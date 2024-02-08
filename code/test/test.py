04254de274f745367e87477c43ea236f23740199
gongyeheyu <gongyeheyu@outlook.com>
2024-02-08 08:20:24 +0800

    up

--- code/test/test.py
+++ code/test/test.py
@@ -15,11 +15,22 @@ def aaa():
 
 def bbb():
     print("bbb")
-options = {
-    1: aaa,
-    2: bbb,}
-i_appscho = int(input("请输入"))
-if i_appscho in options:
-    options[i_appscho]()
-else:
-    input("输入错误")
+#options = {
+#    1: aaa,
+#    2: bbb,}
+#i_appscho = int(input("请输入"))
+#if i_appscho in options:
+#    options[i_appscho]()
+#else:
+#    input("输入错误")
+
+from tqdm import trange
+#for i in trange(100000000):
+#    i = i+1
+#print("done")
+from tqdm import tqdm
+from time import sleep
+bar=tqdm(total=100)
+bar.set_description("Processing1")
+sleep(5)
+bar.update(10)
